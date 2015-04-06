5b8e0a40b60b355dc4af46499177f26b6a1a15a6
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-04-06 12:43:48 +0200

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -17,3 +17,30 @@
  * Output
  * -------------------------------------------------
  */
+
+<?include("inc/functions.php");?>
+<html>
+ <head>
+  <?head("", array("index"));?>
+ </head>
+ <body>
+  <?headerElem();?>
+  <div class="container">
+   <center>
+    <h1>Web Search</h1>
+    <form class="searchForm" action="search.php" method="GET">
+     <input type="text" autocomplete="off" name="q" id="query"/>
+     <div>
+      <button>
+       <svg class='shape-search' viewBox="0 0 100 100" class='shape-search'><use xlink:href='#shape-search'></use></svg>
+      </button>
+     </div>
+     <p>Free, Open Source & Anonymous</p>
+     <p><a href="https://github.com/subins2000/search">GitHub (Source Code)</a></p>
+     <p><a href="about">About</a></p>
+    </form>
+   </center>
+  </div>
+  <?footer();?>
+ </body>
+</html>
