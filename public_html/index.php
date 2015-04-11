e29c82d36dd84f1b4abb409ac0d22941ce45bd77
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-04-11 14:16:28 +0200

    Create index.php

---
+++ public_html/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * test index
+ *
+ * @author Suriyaa Kudo, 2015-present
+ * @license http://suriyaakudo.mit-license.org/
+ * @package wm-search
+ */
+
+/**
+ * Configuration
+ * -------------------------------------------------
+ */
+
+
+/**
+ * Output
+ * -------------------------------------------------
+ */
+
+echo "It works!"
